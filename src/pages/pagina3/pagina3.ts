c945796cef4fa813d5efeb4e24eb71607c9896b5
Treisy Jimenez <treisyj@thehangar.cr>
2017-11-27 11:56:10 -0600

    Navigation with parameters

--- src/pages/pagina3/pagina3.ts
+++ src/pages/pagina3/pagina3.ts
@@ -1,25 +1,18 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the Pagina3Page page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-pagina3',
   templateUrl: 'pagina3.html',
 })
 export class Pagina3Page {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  mutante:any = {};
+
+  constructor(public navCtrl: NavController, private navParams: NavParams) {
+    this.mutante = this.navParams.get("personaje");
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad Pagina3Page');
-  }
+ 
 
 }
